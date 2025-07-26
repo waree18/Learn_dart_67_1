d600bccbd5b91bab5a9ed29077440b0df6ff28ab
waree18 <6749010018@phuketvc.ac.th>
2025-07-26 14:35:09 +0700

    Step 1: Structure

---
+++ dart1.dart
@@ -0,0 +1,24 @@
+// Structure in Dart
+void main() {
+  var name = "John"; // Semi colon
+  print(name);
+
+  print(c);
+  print("Your name is ${name = 'Jane'}.");
+
+  int age = 20;
+  print("Your age is $age.");
+
+  // Good variable names
+  String useNameColorList = "Alice";
+  int _useAge = 12;
+  double temperatureCelsius = 36.6;
+  bool isActive = true;
+
+  // Bad variable names
+  // bool isActive = false; // Duplicate declaration
+  //String 1Name = "Bob"; // Invalid identifier
+  // int maxCount = 50; // Valid identifier
+  // double totalPercent = 100.0; // Valid identifier
+  // var if = "Error"; // reserved keyword
+}
