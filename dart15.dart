fa65104f4877eb22cbfa1969d780a3a1e812afda
waree18 <6749010018@phuketvc.ac.th>
2025-08-02 15:09:37 +0700

    Step 15: Switch case

---
+++ dart15.dart
@@ -0,0 +1,21 @@
+// Switch case
+
+void main() {
+  int number = 2;
+
+  switch (number) {
+    case 1:
+    print("Number is one");
+    break;
+    case 2:
+    print("Number is two");
+    break;
+    case 3:
+    {
+    print("Number is three");
+    break;
+  }
+  default:
+    print("Number is not one, two, or three");
+  }
+  }
